9bbea35eca9976fa294399b21f35e5409a9e00cf
Nicolas Govea <goveanicolas@gmail.com>
2024-02-02 16:38:15 -0600

    trying this

--- sample.js
+++ sample.js
@@ -1,18 +1,19 @@
 const axios = require('axios')
 
-const url = 'https://api.github.com/repos/goveanick/githubActionSlack/dispatches'
+const url = 'https://api.github.com/repos/goveanick/githubActionSlack/dispatches/'
+// const url = 'https://smee.io/zwxaDxa8vn2xWolt'
 
 const postData = {
-    event_type: 'publish_blog'
+    json: {
+        event_type: 'publish_blog'
+    },
+    headers: {
+        Authorization: 'token ' + githubPAT
+    }
 }
 
 // Axios POST request with authorization headers
-axios.post(url, postData, {
-    headers: {
-      'Authorization': `token ${githubPAT}`,
-      'Content-Type': 'application/json', // Adjust the content type based on your API requirements
-    },
-}).then(response => {
+axios.post(url, postData).then(response => {
     console.log('Response:', response.data);
 }).catch(error => {
     console.error('Error:', error.response ? error.response.data : error.message);
