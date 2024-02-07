25f6cf87de15aeddb1c22ffc903b40b1feaa6e20
Nicolas Govea <goveanicolas@gmail.com>
2024-02-07 13:16:40 -0600

    updating sample js file

--- sample.js
+++ sample.js
@@ -2,22 +2,6 @@ const axios = require('axios')
 
 const url = 'https://api.github.com/repos/goveanick/githubActionSlack/dispatches'
 // const url = 'https://smee.io/zwxaDxa8vn2xWolt'
-
-const postData = {
-    json: {
-        event_type: 'publish_blog'
-    },
-    headers: {
-        Authorization: 'token ' + githubPAT
-    }
-}
-
-// Axios POST request with authorization headers
-axios.post(url).then(response => {
-    console.log('Response:', response.data);
-}).catch(error => {
-    console.error('Error:', error.response ? error.response.data : error.message);
-});
   
 
 console.log('hello there again')
